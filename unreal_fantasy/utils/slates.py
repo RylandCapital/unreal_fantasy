eebca512031ae669a1f8a4756fdac1a049618135
Ryland <rylandmathews1@gmail.com>
2023-03-28 15:48:35 -0400

    data collection started

---
+++ unreal_fantasy/utils/slates.py
@@ -0,0 +1,165 @@
+#platform/sport 
+slates = {
+    'fanduel':{
+    
+    'nhl':{
+    
+        '1.6.22': {'day':'thursday', 'slate_id':0, 'winning_score':273.90, 'optimal':337.3},
+        '1.8.22': {'day':'saturday', 'slate_id':1, 'winning_score':251.60, 'optimal':297.8},
+        '1.13.22': {'day':'thursday', 'slate_id':2, 'winning_score':229.00, 'optimal':319.4},
+        '1.15.22': {'day':'saturday', 'slate_id':3, 'winning_score':242.50, 'optimal':302},
+        '1.20.22': {'day':'thursday', 'slate_id':4, 'winning_score':250.50, 'optimal':334},
+        '1.22.22': {'day':'saturday', 'slate_id':5, 'winning_score':229.60, 'optimal':350},
+        '1.27.22': {'day':'thursday', 'slate_id':6, 'winning_score':254.40, 'optimal':322.4},
+        '1.29.22': {'day':'saturday', 'slate_id':7, 'winning_score':221.50, 'optimal':277.8},
+        '2.10.22': {'day':'thursday', 'slate_id':8, 'winning_score':234.40, 'optimal':312.4},
+        '2.12.22': {'day':'saturday', 'slate_id':9, 'winning_score':260.50, 'optimal':307.2},
+        '2.17.22': {'day':'thursday', 'slate_id':10, 'winning_score':230.20, 'optimal':327.6},
+        '2.19.22': {'day':'saturday', 'slate_id':11, 'winning_score':268.30, 'optimal':292},
+        '2.24.22': {'day':'thursday', 'slate_id':12, 'winning_score':248.10, 'optimal':319.8},
+        '2.26.22': {'day':'saturday', 'slate_id':13, 'winning_score':266.50, 'optimal':357.5},
+        '3.3.22': {'day':'thursday', 'slate_id':14, 'winning_score':216.20, 'optimal':351.5},
+        '3.5.22': {'day':'saturday', 'slate_id':15, 'winning_score':247.90, 'optimal':332.2},
+        '3.10.22': {'day':'thursday', 'slate_id':16, 'winning_score':300.60, 'optimal':351},
+        '3.12.22': {'day':'saturday', 'slate_id':17, 'winning_score':232.70, 'optimal':302.8},
+        '3.17.22': {'day':'thursday', 'slate_id':18, 'winning_score':255.50, 'optimal':324.7},
+        '3.19.22': {'day':'saturday', 'slate_id':19, 'winning_score':216.70, 'optimal':265.8},
+        '3.24.22': {'day':'thursday', 'slate_id':20, 'winning_score':253.00, 'optimal':322.5},
+        '3.26.22': {'day':'saturday', 'slate_id':21, 'winning_score':247.60, 'optimal':343.1},
+        '3.31.22': {'day':'thursday', 'slate_id':22, 'winning_score':228.40, 'optimal':306.1},
+        '4.2.22': {'day':'saturday', 'slate_id':23, 'winning_score':239.30, 'optimal':309.5},
+        '4.9.22': {'day':'saturday', 'slate_id':24, 'winning_score':268.90, 'optimal':352.9},
+        '4.14.22': {'day':'thursday', 'slate_id':25, 'winning_score':287.20, 'optimal':369.7},
+        '4.16.22': {'day':'saturday', 'slate_id':26, 'winning_score':238.40, 'optimal':350.8},
+        '10.20.22': {'day':'thursday', 'slate_id':29, 'winning_score':242.20, 'optimal':356.2},
+        '10.22.22': {'day':'saturday', 'slate_id':30, 'winning_score':242.20, 'optimal':322.2}, 
+        '10.27.22': {'day':'thursday', 'slate_id':31, 'winning_score':280.20, 'optimal':336.8},  
+        '10.29.22': {'day':'saturday', 'slate_id':32, 'winning_score':235.90, 'optimal':325.1}, 
+        '11.10.22': {'day':'thursday', 'slate_id':35, 'winning_score':281.80, 'optimal':363.1}, 
+        '11.12.22': {'day':'saturday', 'slate_id':36, 'winning_score':196.70, 'optimal':296.1},
+        '11.17.22': {'day':'thursday', 'slate_id':37, 'winning_score':279.60, 'optimal':363}, 
+        '11.19.22': {'day':'saturday', 'slate_id':38, 'winning_score':286.10, 'optimal':336.3},
+        '12.1.22': {'day':'thursday', 'slate_id':39, 'winning_score':258.40, 'optimal':354.9}, 
+        '12.3.22': {'day':'saturday', 'slate_id':40, 'winning_score':234.80, 'optimal':327.6},
+        '12.8.22': {'day':'thursday', 'slate_id':41, 'winning_score':219.40, 'optimal':264.2}, 
+        '12.10.22': {'day':'saturday', 'slate_id':42, 'winning_score':233.40, 'optimal':286.4},
+        '12.15.22': {'day':'thursday', 'slate_id':43, 'winning_score':232.50, 'optimal':295.6}, 
+        '12.17.22': {'day':'saturday', 'slate_id':44, 'winning_score':222.70, 'optimal':318.6},
+        '12.22.22': {'day':'thursday', 'slate_id':45, 'winning_score':211.10, 'optimal':297.8},
+        '1.5.23': {'day':'thursday', 'slate_id':46, 'winning_score':245.60, 'optimal':325.3},
+        '1.7.23': {'day':'saturday', 'slate_id':47, 'winning_score':238.30, 'optimal':338.6},
+        '1.10.23': {'day':'tuesday', 'slate_id':48, 'winning_score':241.00, 'optimal':329.6},
+        '1.12.23': {'day':'thursday', 'slate_id':49, 'winning_score':212.25, 'optimal':298.2},
+        '1.14.23': {'day':'saturday', 'slate_id':50, 'winning_score':217.70, 'optimal':339},
+        '1.17.23': {'day':'tuesday', 'slate_id':51, 'winning_score':207.10, 'optimal':324.6},
+        '1.19.23': {'day':'thursday', 'slate_id':52, 'winning_score':227.00, 'optimal':315.1},
+        '1.21.23': {'day':'saturday', 'slate_id':53, 'winning_score':193.00, 'optimal':314.2},
+        '1.24.23': {'day':'tuesday', 'slate_id':54, 'winning_score':243.00, 'optimal':345.2},
+        '1.26.23': {'day':'thursday', 'slate_id':55, 'winning_score':231.00, 'optimal':355.2},
+        '2.7.23': {'day':'tuesday', 'slate_id':56, 'winning_score':209.00, 'optimal':273.2},
+        '2.9.23': {'day':'thursday', 'slate_id':57, 'winning_score':231.00, 'optimal':309.8},
+        '2.17.23': {'day':'friday', 'slate_id':58, 'winning_score':237.00, 'optimal':301.4},
+        '2.18.23': {'day':'saturday', 'slate_id':59, 'winning_score':258.00, 'optimal':332.8},
+        '2.21.23': {'day':'tuesday', 'slate_id':60, 'winning_score':251.00, 'optimal':332.3},
+        '2.23.23': {'day':'thursday', 'slate_id':61, 'winning_score':244.00, 'optimal':342.9},
+        '2.25.23': {'day':'saturday', 'slate_id':62, 'winning_score':183.00, 'optimal':262.4},
+        '2.28.23': {'day':'tuesday', 'slate_id':63, 'winning_score':244.00, 'optimal':381.9},
+        '3.2.23': {'day':'thursday', 'slate_id':64, 'winning_score':226.00, 'optimal':319.9},
+        '3.4.23': {'day':'saturday', 'slate_id':65, 'winning_score':222.00, 'optimal':311},
+        '3.7.23': {'day':'tuesday', 'slate_id':66, 'winning_score':260.00, 'optimal':349.4},
+        '3.9.23': {'day':'thursday', 'slate_id':67, 'winning_score':215, 'optimal':314},
+        '3.11.23': {'day':'saturday', 'slate_id':68, 'winning_score':230, 'optimal':352},
+        '3.14.23': {'day':'tuesday', 'slate_id':69, 'winning_score':260, 'optimal':353.9},
+        '3.16.23': {'day':'thursday', 'slate_id':70, 'winning_score':224.10, 'optimal':327.4},
+        '3.18.23': {'day':'saturday', 'slate_id':71, 'winning_score':252, 'optimal':348.9}, 
+        '3.20.23': {'day':'monday', 'slate_id':72, 'winning_score':220.20, 'optimal':293},
+        '3.21.23': {'day':'tuesday', 'slate_id':73, 'winning_score':231.00, 'optimal':349.4},
+        '3.23.23': {'day':'thursday', 'slate_id':74, 'winning_score':191.3, 'optimal':282.9}, 
+        '3.25.23': {'day':'saturday', 'slate_id':75, 'winning_score':271.60, 'optimal':329.50},
+        '3.27.23': {'day':'tuesday', 'slate_id':76, 'winning_score':227.30, 'optimal':300.4},
+
+    },
+
+    'nfl':{
+    
+        '9.4.19': {'day':'sunday',  'slate_id':1, 'winning_score':241.96, 'optimal':291.7},  #week 1 2019
+        '9.11.19': {'day':'sunday', 'slate_id':2, 'winning_score':205.32, 'optimal':239.2},  #week 2 2019
+        '9.18.19': {'day':'sunday', 'slate_id':3, 'winning_score':238.54, 'optimal':277.9},  #week 3 2019
+        '9.25.19': {'day':'sunday', 'slate_id':4, 'winning_score':216.79, 'optimal':245.3},  #week 4 2019
+        '10.2.19': {'day':'sunday', 'slate_id':5, 'winning_score':285.64, 'optimal':325.4},  #week 5 2019
+        '10.9.19': {'day':'sunday', 'slate_id':6, 'winning_score':215.80, 'optimal':239.0},  #week 6 2019
+        '10.16.19': {'day':'sunday', 'slate_id':7, 'winning_score':214.86, 'optimal':268.8}, #week 7 2019
+        '10.23.19': {'day':'sunday', 'slate_id':8, 'winning_score':225.86, 'optimal':260.4}, #week 8 2019
+        '10.30.19': {'day':'sunday', 'slate_id':9, 'winning_score':233.52, 'optimal':245.7}, #week 9 2019
+        '11.6.19': {'day':'sunday', 'slate_id':10, 'winning_score':218.92, 'optimal':251.0}, #week 10 2019
+        '11.13.19': {'day':'sunday', 'slate_id':11, 'winning_score':197.74, 'optimal':221.8},#week 11 2019
+        '11.20.19': {'day':'sunday', 'slate_id':12, 'winning_score':225.18, 'optimal':250.5},#week 12 2019
+        '11.27.19': {'day':'sunday', 'slate_id':13, 'winning_score':188.66, 'optimal':221.8},#week 13 2019
+        '12.4.19': {'day':'sunday', 'slate_id':14, 'winning_score':206.46, 'optimal':247.7}, #week 14 2019
+        '12.11.19': {'day':'sunday', 'slate_id':15, 'winning_score':232.00, 'optimal':262.9},#week 15 2019
+        '12.18.19': {'day':'sunday', 'slate_id':16, 'winning_score':234.00, 'optimal':267.8},#week 16 2019
+        '17':215.46, #week 1 2020
+        '18':223.60, #week 2 2020
+        '19':214.90, #week 3 2020
+        '20':232.00, #week 4 2020
+        '21':205.00, #week 5 2020
+        '22':208.00, #week 6 2020
+        '23':235.00, #week 7 2020
+        '24':211.24, #week 8 2020
+        '25':201.16, #week 9 2020
+        '26':196.06, #week 10 2020
+        '27':187.96, #week 11 2020
+        '28':230, #week 12 2020
+        '29':220, #week 13 2020
+        '30':220, #week 14 2020 
+        '31':199, #week 1 2021
+        '32':227, #week 2 2021
+        '33':204, #week 3 2021
+        '34':233, #week 4 2021
+        '35':231, #week 5 2021
+        '36':210, #week 6 2021 
+        '37':208, #week 7 2021 FL: 10/20/21
+        '38':190, #week 8 2021 FL: 10/27/21 
+        '39':196, #week 9 2021 FL: 11/03/21
+        '40':184, #week 10 2021 FL: 11/10/21
+        '41':221, #week 11 2021 FL: 11/17/21
+        '42':196, #week 12 2021 FL: 11/24/21
+        '43':219, #week 13 2021 FL: 12/01/21
+        '44':201, #week 14 2021 FL: 12/08/21
+        '45':186, #week 15 2021 FL: 12/15/21
+        '46':221, #week 1 2022 FL: 9/7/22
+        '47':245, #week 2 2022 FL: 9/14/22
+        '48':190, #week 3 2022 FL: 9/21/22
+        '49':228, #week 4 2022 FL: 9/28/22
+        '50':228, #week 5 2022 FL: 10/5/22
+        '51':198, #week 6 2022 FL: 10/12/22  
+        '52':215, #week 7 2022 FL: 10/19/22 
+        '53':246, #week 8 2022 FL: 10/26/22
+        '54':230, #week 9 2022 FL: 11/2/22
+        '55':219, #week 10 2022 FL: 11/9/22 
+        '56':188, #week 11 2022 FL: 11/16/22 
+        '57':199, #week 12 2022 FL: 11/23/22 
+        '58':197, #week 13 2022 FL: 11/30/22 
+        '59':212,  #week 14 2022 FL: 12/7/22
+        '60':198  #week 15 2022 FL: 12/14/22
+    
+    }
+
+    },
+
+    'draftkings':{
+    
+    'nhl':{
+    
+    },
+
+    'nfl':{
+    
+    }
+
+    },
+
+
+
+
+}
